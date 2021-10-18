0992c8260bc4752c9fb57591b9c3bd60ec908b77
SAYEED AJMAL <SAYEEDAJMALA06@GMAIL.COM>
2021-10-18 18:39:08 +0530

    updated

--- Main.php
+++ Main.php
@@ -10,25 +10,24 @@ include "Include/Header.php";
     </header>
 
     <section>
-        <!-- SOME PARAGRAPH -->
         <div class="container">
             <text>Hi Devs, I'm Sayeed Ajmal. I am Front-End & Back-End Developer. Here's My All Projects.</text>
         </div>
 
         <div class="center">
-            <div class="first_product">
+            <div class="first_application">
                 <h1>01</h1>
                 <p>BANKING SYSTEM</p>
                 <i class="far fa-circle" id="circle"></i>
 
             </div>
-            <div class="second_product">
+            <div class="second_application">
                 <h1>02</h1>
                 <p>BIODATA</p>
                 <i class="far fa-heart" id="circle"></i>
 
             </div>
-            <div class="third_product">
+            <div class="third_application">
                 <h1>03</h1>
                 <p>PERFECTION JAVA</p>
                 <i class="far fa-square" id="circle"></i>
@@ -37,45 +36,45 @@ include "Include/Header.php";
 
         </div>
 
-        <div class="hill">
+        <div class="gitback">
             <img src="/IMAGES/gitback.png" />
             <h1>You Can Contribute in My Project.</h1>
             <h4>Genius Developers Contribute in Projects. Because Contributing In Projects Make You Perfect. .</h4>
             <input type="button" value="Connect With Me" />
         </div>
-        <div class="shop_product">
+        <div class="devs_list">
             <h5>Developer's List</h5>
             <h2>Connect with Devs</h2>
         </div>
-        <div class="clothes">
-            <div class="cloth_names">
+        <div class="devs">
+            <div class="devs_information">
                 <img src="/IMAGES/coat.png">
-                <h4>Coat</h4>
-                <input type="button" value="Details" />
+                <h4>Sayeed Ajmal</h4>
+                <input type="button" value="Connect" />
             </div>
 
-            <div class="cloth_names">
+            <div class="devs_information">
                 <img src="/IMAGES/geans.jpg">
-                <h4>Geans</h4>
-                <input type="button" value="Details" />
+                <h4>Hansy Gill</h4>
+                <input type="button" value="Connect" />
             </div>
 
-            <div class="cloth_names">
+            <div class="devs_information">
                 <img src="/IMAGES/white.png">
-                <h4>T-Shirt</h4>
-                <input type="button" value="Details" />
+                <h4>Md Imdadullah</h4>
+                <input type="button" value="Connect" />
             </div>
         </div>
-        <input class="all_products" type="button" value="View All Products" />
+        <input class="all_devs" type="button" value="Wanna More Developers" />
         <img class="my_pic" src="/IMAGES/sayeed.png">
-        <div class="shop_container">
-            <div class="shop_local">
-                <h1>SHOP LOCAL</h1>
+        <div class="running_project">
+            <div class="working_project">
+                <h1>PERFECTION IN JAVA</h1>
                 <p>We know that during COVID-19, a lot of folks around the city and state are feeling uneasy about the future - we’re not sure what the future holds either.</p>
                 <p>That said: we know that we love making sure you have the gear you need for your adventures, and we’re going to keep doing that - with our team - until the city tells us we can’t.</p>
                 <p>But as long as folks like yourself support small businesses around the city, then we’ll be here — every day, making sure your orders arrive on time.</p>
                 <text class="name">Sayeed Ajmal</text>
-                <p>Special For SomeOne</p>
+                <p>Special For Devs.</p>
             </div>
         </div>
     </section>
